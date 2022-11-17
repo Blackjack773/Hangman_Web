18f6867f5bca52d6d07b1be8235e451e9feb32a6
chocine <hocine.cauchoix@ynov.com>
2022-11-17 15:58:24 +0100

    frkeve

--- main.go
+++ main.go
@@ -10,11 +10,7 @@ const port = ":3000"
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-<<<<<<< HEAD
 	http.HandleFunc("/hangman", Hangman)
-=======
-	http.HandleFunc("/index/Game", Game)
->>>>>>> 1079b051b98ceef7d4467eafe15a11f3642d1ec9
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./html/template/css/"))))
 	fmt.Println("http://localhost:3000 - Server started on port :3000")
 
