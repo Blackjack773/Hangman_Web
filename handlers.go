cc178b3db53252cd45c3723c184e0ed02a7c082b
ljacques <jacqueslao03@gmail.com>
2022-11-24 08:59:56 +0100

    added stylesheet

--- handlers.go
+++ handlers.go
@@ -1,27 +1,31 @@
 package main
 
 import (
-	"net/http"
-	"text/template"
+    "net/http"
+    "text/template"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index")
+func Home(w http.ResponseWriter, r http.Request) {
+    renderTemplate(w, "index")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about")
+func About(w http.ResponseWriter, rhttp.Request) {
+    renderTemplate(w, "about")
 }
 
 func Hangman(w http.ResponseWriter, r *http.Request) {
+<<<<<<< Updated upstream
 	renderTemplate(w, "hangman")
+=======
+    renderTemplate(w, "hangman")
+>>>>>>> Stashed changes
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./html/template/" + tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
+    t, err := template.ParseFiles("./html/template/" + tmpl + ".html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    t.Execute(w, nil)
 }
