65dc8e231f36ec5538ad4b50ebfc316312f92b5a
ljacques <jacqueslao03@gmail.com>
2022-12-03 17:02:48 +0100

    Link Page Elisa

--- handlers.go
+++ handlers.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"text/template"
+    "net/http"
+    "text/template"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index")
+    renderTemplate(w, "index")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about")
+    renderTemplate(w, "about")
 }
 
 func Hangman(w http.ResponseWriter, r *http.Request) {
@@ -18,10 +18,10 @@ func Hangman(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./html/template/" + tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
+    t, err := template.ParseFiles("./html/template/" + tmpl + ".html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    t.Execute(w, nil)
 }
